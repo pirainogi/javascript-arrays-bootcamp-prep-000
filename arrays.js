76c47ce0543af6b77edc16f873ad653ae271fca5
Gabbie Piraino <pirainogi@gmail.com>
2018-11-04 19:16:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,9 @@ var chocString = "foo";
 function addElementToBeginningOfArray(chocolateBars, chocString){
   return [chocString, ...chocolateBars];
 }
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, chocString){
+  chocolateBars.unshift(chocString);
+  return chocolateBars;
+}
+
