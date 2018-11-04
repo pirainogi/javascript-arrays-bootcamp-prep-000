17388bbdf8a8df3d2a8b517cec15cbba7a0b2655
Gabbie Piraino <pirainogi@gmail.com>
2018-11-04 19:42:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,4 +23,5 @@ function destructivelyAddElementToEndOfArray(chocolateBars, chocString){
 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
   chocolateBars.shift();
+  return chocolateBars;
 }
