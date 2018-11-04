b473e933ab769ffe0a80040189a02da7cdcc5b17
Gabbie Piraino <pirainogi@gmail.com>
2018-11-04 19:47:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,3 +29,5 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 function removeElementFromBeginningOfArray(chocolateBars){
   return chocolateBars.slice(1);
 }
+
+function 
