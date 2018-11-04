37c0e2ec2c7102f92821a0f635183bda8d736f93
Gabbie Piraino <pirainogi@gmail.com>
2018-11-04 19:16:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]; 
 var chocString = "foo";
+
+function addElementToBeginningOfArray(chocolateBars, chocString){
+  return [chocString, ...chocolateBars];
+}
